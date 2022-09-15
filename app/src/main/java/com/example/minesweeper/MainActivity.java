d138f0874f3ab2a4c20f603bc28a0840fc9171e3
Tyler Sehon <tsehon@usc.edu>
2022-09-14 17:11:05 -0700

    added gridlayout and positioning for timer and flag elements

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -2,13 +2,139 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.widget.GridLayout;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ROW_COUNT = 10;
+    private static final int COLUMN_COUNT = 8;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int timer = 0;
+    private boolean timer_running = false;
+    private boolean first_click = true;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        timer = 0;
+        timer_running = false;
+        first_click = true;
+        */
+
+        if (savedInstanceState != null) {
+            timer = savedInstanceState.getInt("timer");
+            timer_running = savedInstanceState.getBoolean("timer_running");
+            first_click = savedInstanceState.getBoolean("first_click");
+        }
+
+        cell_tvs = new ArrayList<TextView>();
+
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout0);
+
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j = 0; j < COLUMN_COUNT; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight(dpToPixel(32));
+                tv.setWidth(dpToPixel(32));
+                tv.setTextSize(18);
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickGridCell);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
     }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putInt("timer", timer);
+        savedInstanceState.putBoolean("timer_running", timer_running);
+        savedInstanceState.putBoolean("first_click", first_click);
+    }
+
+    private void runClock() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int seconds = timer%60;
+                String time = String.format("%02d", seconds);
+                timeView.setText(time);
+
+                if (timer_running) {
+                    timer++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    public void onClickGridCell(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+
+        int i = n/COLUMN_COUNT;
+        int j = n%COLUMN_COUNT;
+        tv.setText(String.valueOf(i)+String.valueOf(j));
+
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        } else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+
+        if (first_click) {
+            timer_running = true;
+            runClock();
+            first_click = false;
+        }
+    }
+
+
+
+
+
 }
